a8796f53f85dddc126c7878fae73908ee0b20f21
Firoz-PS <ce19b048@smail.iitm.ac.in>
2020-09-26 11:23:43 +0530

    Updates

---
+++ precache-manifest.8339aa28242fa2602109d467c8b11c76.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f0c7ddbb5a27794805e35184625d6d",
+    "url": "/tensorsiitm/Tensors-EDU.git/index.html"
+  },
+  {
+    "revision": "0d70d23f2de5f6c9f2e5",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/css/2.9bb90de5.chunk.css"
+  },
+  {
+    "revision": "4601b765f59b534d3deb",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/css/main.f3948ccd.chunk.css"
+  },
+  {
+    "revision": "0d70d23f2de5f6c9f2e5",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/js/2.2dc72494.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/js/2.2dc72494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4601b765f59b534d3deb",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/js/main.7ec9b15b.chunk.js"
+  },
+  {
+    "revision": "c59498ebdcfa2d4e152e",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/js/runtime-main.4cc3a5ac.js"
+  },
+  {
+    "revision": "d6dbad2d3c8bb820cb0d5b78300c8470",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a1.d6dbad2d.svg"
+  },
+  {
+    "revision": "f40e55661fdd2ca9bfa40f234db26643",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a1b.f40e5566.svg"
+  },
+  {
+    "revision": "62cf229458f2576941fdf7bd87661228",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a2.62cf2294.svg"
+  },
+  {
+    "revision": "b84dcb5cd6d299575d607ec24fcbe9ed",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a2b.b84dcb5c.svg"
+  },
+  {
+    "revision": "e9f6656b0d2b10282589643ce094d6ea",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a3.e9f6656b.svg"
+  },
+  {
+    "revision": "a4b73dcb67326c10c0f2570850e8a3df",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a3b.a4b73dcb.svg"
+  },
+  {
+    "revision": "8086febecf288ea34e98f3cd03d1c199",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a4.8086febe.svg"
+  },
+  {
+    "revision": "b84dcb5cd6d299575d607ec24fcbe9ed",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/a4b.b84dcb5c.svg"
+  },
+  {
+    "revision": "71540644f433839957be5a05a5809380",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/aghin.71540644.jpeg"
+  },
+  {
+    "revision": "613f8b37aafde53bd30c94c3397ed0f6",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/anand.613f8b37.jpg"
+  },
+  {
+    "revision": "d66569398c3628adc3b95312afa74a0e",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/construction.d6656939.svg"
+  },
+  {
+    "revision": "7cf3bf1d5bf68053e2ddc83d553a36bc",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/denin.7cf3bf1d.jpeg"
+  },
+  {
+    "revision": "44499462e8c0607d76c7bf7b6a655af6",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/favicon.44499462.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4561225b9c6885a2ccd1bfe0b0a74e1b",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/shafil.4561225b.jpg"
+  },
+  {
+    "revision": "870a1c9cd17d0692120e5d28c9d6ff13",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/stripes.870a1c9c.svg"
+  },
+  {
+    "revision": "df958144c72111c48d73fbb3a179ee21",
+    "url": "/tensorsiitm/Tensors-EDU.git/static/media/tcloud.df958144.svg"
+  }
+]);
