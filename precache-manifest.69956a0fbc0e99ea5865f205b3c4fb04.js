568e1dfefd3fcf89df37a3dd82c51b1c69f44c90
Firoz-PS <ce19b048@smail.iitm.ac.in>
2020-09-26 11:29:47 +0530

    Updates

---
+++ precache-manifest.69956a0fbc0e99ea5865f205b3c4fb04.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecdb679223e721672169c0e5d9c6e8c",
+    "url": "/Tensors-EDU/index.html"
+  },
+  {
+    "revision": "bb36a90e533bbaef5d8d",
+    "url": "/Tensors-EDU/static/css/2.5ce1ef0a.chunk.css"
+  },
+  {
+    "revision": "c94cf161d8e2d836faf7",
+    "url": "/Tensors-EDU/static/css/main.b6ceaf4b.chunk.css"
+  },
+  {
+    "revision": "bb36a90e533bbaef5d8d",
+    "url": "/Tensors-EDU/static/js/2.2dc72494.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Tensors-EDU/static/js/2.2dc72494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94cf161d8e2d836faf7",
+    "url": "/Tensors-EDU/static/js/main.85f59da6.chunk.js"
+  },
+  {
+    "revision": "4890ea5b5daacfd6f776",
+    "url": "/Tensors-EDU/static/js/runtime-main.eafeff03.js"
+  },
+  {
+    "revision": "d6dbad2d3c8bb820cb0d5b78300c8470",
+    "url": "/Tensors-EDU/static/media/a1.d6dbad2d.svg"
+  },
+  {
+    "revision": "f40e55661fdd2ca9bfa40f234db26643",
+    "url": "/Tensors-EDU/static/media/a1b.f40e5566.svg"
+  },
+  {
+    "revision": "62cf229458f2576941fdf7bd87661228",
+    "url": "/Tensors-EDU/static/media/a2.62cf2294.svg"
+  },
+  {
+    "revision": "b84dcb5cd6d299575d607ec24fcbe9ed",
+    "url": "/Tensors-EDU/static/media/a2b.b84dcb5c.svg"
+  },
+  {
+    "revision": "e9f6656b0d2b10282589643ce094d6ea",
+    "url": "/Tensors-EDU/static/media/a3.e9f6656b.svg"
+  },
+  {
+    "revision": "a4b73dcb67326c10c0f2570850e8a3df",
+    "url": "/Tensors-EDU/static/media/a3b.a4b73dcb.svg"
+  },
+  {
+    "revision": "8086febecf288ea34e98f3cd03d1c199",
+    "url": "/Tensors-EDU/static/media/a4.8086febe.svg"
+  },
+  {
+    "revision": "b84dcb5cd6d299575d607ec24fcbe9ed",
+    "url": "/Tensors-EDU/static/media/a4b.b84dcb5c.svg"
+  },
+  {
+    "revision": "71540644f433839957be5a05a5809380",
+    "url": "/Tensors-EDU/static/media/aghin.71540644.jpeg"
+  },
+  {
+    "revision": "613f8b37aafde53bd30c94c3397ed0f6",
+    "url": "/Tensors-EDU/static/media/anand.613f8b37.jpg"
+  },
+  {
+    "revision": "d66569398c3628adc3b95312afa74a0e",
+    "url": "/Tensors-EDU/static/media/construction.d6656939.svg"
+  },
+  {
+    "revision": "7cf3bf1d5bf68053e2ddc83d553a36bc",
+    "url": "/Tensors-EDU/static/media/denin.7cf3bf1d.jpeg"
+  },
+  {
+    "revision": "44499462e8c0607d76c7bf7b6a655af6",
+    "url": "/Tensors-EDU/static/media/favicon.44499462.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Tensors-EDU/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Tensors-EDU/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Tensors-EDU/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Tensors-EDU/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Tensors-EDU/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4561225b9c6885a2ccd1bfe0b0a74e1b",
+    "url": "/Tensors-EDU/static/media/shafil.4561225b.jpg"
+  },
+  {
+    "revision": "870a1c9cd17d0692120e5d28c9d6ff13",
+    "url": "/Tensors-EDU/static/media/stripes.870a1c9c.svg"
+  },
+  {
+    "revision": "df958144c72111c48d73fbb3a179ee21",
+    "url": "/Tensors-EDU/static/media/tcloud.df958144.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tensorsiitm/Tensors-EDU.git/precache-manifest.8339aa28242fa2602109d467c8b11c76.js"
+  "/Tensors-EDU/precache-manifest.69956a0fbc0e99ea5865f205b3c4fb04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tensorsiitm/Tensors-EDU.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tensors-EDU/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
