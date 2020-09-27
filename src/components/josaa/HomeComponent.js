ec0ffb17a4ad93f06ec5b82397d4ede934bcba0b
hadi-pv <chumma1357@gmail.com>
2020-09-27 22:39:58 +0530

    hadi6

--- src/components/josaa/HomeComponent.js
+++ src/components/josaa/HomeComponent.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import './josaa.css';
+import {Jumbotron} from 'reactstrap';
 import { Parallax } from 'react-scroll-parallax';
+
 const Home=()=>{
     return(
-        <div className="container-fluid mt-3 mb-3 home-register ">
+        <div className="container-fluid mt-3 mb-3 home">
             <div className="row">
-                <div className="col-md-6 col-12">
-                    <Parallax className="custom-class d-block d-md-none" z={[30, -20]} tagOuter="figure1">
+                <Jumbotron className="mt-3 jumbotron" >
+                    <div className="container-fluid">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6 mr-auto info">
+                                <h1>JOSAA COUNCELING</h1>
+                                <p className="d-none d-sm-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+            </div>
+            <div className="container">
+                <div className="row" style={{minHeight:'50vh'}}>
+                    <div className="col-md-6 col-12">
                         <h1 style={{position:'relative'}}>Who Are We?</h1>
                         <p style={{position:'relative'}}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.                 
-                        </p>    
-                    </Parallax>
-                    <div className="d-none d-md-block">
-                        <h1 style={{position:'relative'}}>Who Are We?</h1>
-                        <p style={{position:'relative'}}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.                 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                         </p>
                     </div>
-                </div>
-                <div className="col-md-6 col-12 mt-auto mb-auto">
-                    <center>
-                        <button className="home-register-button ml-auto bg-danger">
-                            REGISTRATION
-                        </button>   
-                    </center>
-                    
-                </div>
+                    <div className="col-md-6 col-12 mt-auto mb-auto">
+                        <center>
+                            <button className="home-register-button ml-auto bg-danger">
+                                REGISTRATION
+                            </button>   
+                        </center>
+                    </div>
+                </div>    
             </div>
         </div>
     );
--- src/components/josaa/MainComponent.js
+++ src/components/josaa/MainComponent.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import TimelinePage from './TimelineComponent';
 import Features from './FeaturesComponent';
-import CarouselPage from './CarouselComponent';
-import CardExample from './ConsultantsComponent';
-import Packages from './FlipCardComponent';
+import CarouselPage from './ConsultantCarouselComponent';
+import Packages from './PackageCardComponent';
 import Home from './HomeComponent';
 
-import {Jumbotron} from 'reactstrap';
 import './josaa.css';
 import Img from '../../assets/images/homebg.jpg';
 import {consultants} from '../../assets/data/consultants';
@@ -26,16 +24,6 @@ class JosaaMain extends Component{
             <React.Fragment>
                 <ParallaxProvider>
                     <div className="main container-fluid ">
-                        <Jumbotron className="mt-3 jumbotron " >
-                            <div className="container-fluid">
-                                <div className="row row-header">
-                                    <div className="col-12 col-sm-6 mr-auto info">
-                                        <h1>JOSAA COUNCELING</h1>
-                                        <p className="d-none d-sm-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </Jumbotron>
                         <Home />
                         <Features/>
                         <TimelinePage />
