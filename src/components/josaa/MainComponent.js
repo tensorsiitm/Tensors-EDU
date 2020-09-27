842410f72bfa6b76d59691394312416d02a775c5
hadi-pv <chumma1357@gmail.com>
2020-09-27 20:54:41 +0530

    hadi5

--- src/components/josaa/MainComponent.js
+++ src/components/josaa/MainComponent.js
@@ -10,7 +10,7 @@ import {Jumbotron} from 'reactstrap';
 import './josaa.css';
 import Img from '../../assets/images/homebg.jpg';
 import {consultants} from '../../assets/data/consultants';
-import { ParallaxProvider,Parallax } from 'react-scroll-parallax';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 class JosaaMain extends Component{
@@ -30,10 +30,8 @@ class JosaaMain extends Component{
                             <div className="container-fluid">
                                 <div className="row row-header">
                                     <div className="col-12 col-sm-6 mr-auto info">
-                                    <Parallax className="custom-class" y={[100, -50]} tagOuter="figure">
                                         <h1>JOSAA COUNCELING</h1>
                                         <p className="d-none d-sm-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    </Parallax>
                                     </div>
                                 </div>
                             </div>
