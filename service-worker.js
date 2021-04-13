988fecc0f28986646884ab6126082bea63b510ed
Firoz-PS <ce19b048@smail.iitm.ac.in>
2021-04-13 17:22:20 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21f2f3a8a5301b84b29d4b523b5360d2.js"
+  "/precache-manifest.fc5ca7bd24694164da7f58a2e902ab6c.js"
 );
 
 self.addEventListener('message', (event) => {
