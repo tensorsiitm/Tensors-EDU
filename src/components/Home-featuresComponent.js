0c6db9410a4d021c45d61783b4c64b6ab4225f04
Firoz-PS <ce19b048@smail.iitm.ac.in>
2022-08-18 19:43:16 +0530

    added jee adv 2022 poster

--- src/components/Home-featuresComponent.js
+++ src/components/Home-featuresComponent.js
@@ -1,26 +1,25 @@
 import React from 'react';
-//import { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import a1 from "../assets/images/a1.svg";
 import a2 from "../assets/images/a2.svg";
 import a3 from "../assets/images/a3.svg";
 import a4 from "../assets/images/a4.svg";
 import tlogo from "../assets/images/logof.png";
-//import poster from "../assets/images/exam2022.png";
+import poster from "../assets/images/adv2022.jpg";
 import "../css/Home-featuresComponent.css";
 import { Parallax } from "react-scroll-parallax";
 //import { Link } from "react-scroll";
-//import { Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 
 function Homefeatures() {
-    //const [show, setShow] = useState(true);
+    const [show, setShow] = useState(true);
 
-    //const handleClose = () => setShow(false);
+    const handleClose = () => setShow(false);
   
     return (
       <React.Fragment>
-        {/*
       <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title> </Modal.Title>
@@ -34,7 +33,6 @@ function Homefeatures() {
             </NavLink>
           </Modal.Footer>
         </Modal>
-      */}
 
         <div className="particlebg">
           <div className="row">
